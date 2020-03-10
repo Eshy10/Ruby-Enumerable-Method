67bf65dbd672e15d804a3b817147b27d09ad9d5f
modupe <adewalebose15@gmail.com>
2020-03-10 10:24:17 +0100

    Added each method to module enumerable

--- enumerable-method.rb
+++ enumerable-method.rb
@@ -1,2 +1,14 @@
 module Enumerable
+    def my_each
+        i = 0
+       loop  do
+            if i < self.length
+          yield self[i] if block_given?
+          i += 1
+      else
+          break
+        end
+    end
+    self
+      end
 end
