b93c4cc59844b2767bc38ba9b1521c785d6ac027
modupe <adewalebose15@gmail.com>
2020-03-10 10:27:55 +0100

    Added each_with_index method

--- enumerable-method.rb
+++ enumerable-method.rb
@@ -11,4 +11,17 @@ module Enumerable
     end
     self
       end
+
+      def my_each_with_index
+        i = 0
+       loop  do
+            if i < self.length
+          yield self[i], i if block_given?
+          i += 1
+      else
+          break
+        end
+    end
+    self
+      end
 end
