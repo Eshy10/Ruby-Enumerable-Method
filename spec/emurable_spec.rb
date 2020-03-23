e31a0bb5530313d5259a9d19eb8178980c485c1a
modupe <adewalebose15@gmail.com>
2020-03-23 15:51:57 +0100

    Added my_each_with_index to rspec

--- spec/emurable_spec.rb
+++ spec/emurable_spec.rb
@@ -6,4 +6,10 @@ describe Enumerable do
           expect(([2, 4, 6]).my_each { |num| puts num}).to eq([2, 4, 6])
       end
     end
+
+    describe "my_each_with_index" do
+        it "should return each element in an array with their index" do
+            expect((['apple', 'kiwi', 'grapes']).my_each_with_index { |val, index| puts "#{val} => #{index}"}).to eq(['apple', 'kiwi', 'grapes'])
+        end
+    end
 end
