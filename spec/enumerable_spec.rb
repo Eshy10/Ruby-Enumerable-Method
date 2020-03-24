6b66446715f84bf942f36e09e28e7a6b5cba8a9b
modupe <adewalebose15@gmail.com>
2020-03-24 08:48:09 +0100

    Added a false conditions to the methods in rspec

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -6,11 +6,19 @@ describe Enumerable do
     it 'should return each element in an array' do
       expect([2, 4, 6].my_each { |num| puts num }).to eq([2, 4, 6])
     end
+
+    it 'should not return each element not in the array' do
+      expect([2, 4, 6].my_each { |num| puts num }).not_to eq([2, 4, 8])
+    end
   end
 
   describe 'my_each_with_index' do
     it 'should return each element in an array with their index' do
       expect(%w[apple kiwi grapes].my_each_with_index { |val, index| puts "#{val} => #{index}" }).to eq(%w[apple kiwi grapes])
+    end
+
+    it 'should not change the arrangement of the element and their indexes in the array' do
+      expect(%w[apple kiwi grapes].my_each_with_index { |val, index| puts "#{val} => #{index}" }).not_to eq(%w[apple grapes kiwi])
     end
   end
 
@@ -18,6 +26,11 @@ describe Enumerable do
     it 'should return a new array that satisfy the condition in the block' do
       expect([1, 2, 3, 4, 5, 6].my_select(&:even?)).to eq([2, 4, 6])
       expect([1, 2, 3, 4, 5, 6].my_select(&:odd?)).to eq([1, 3, 5])
+    end
+
+    it 'should not return a new array that does not satisfy the condition in the block' do
+      expect([1, 2, 3, 4, 5, 6].my_select(&:even?)).not_to eq([1, 2, 3, 4, 5, 6])
+      expect([1, 2, 3, 4, 5, 6].my_select(&:odd?)).not_to eq([2, 4, 6])
     end
   end
 
@@ -112,6 +125,12 @@ describe Enumerable do
       expect([3, 5, 3, 7].my_count(3)).to eq(2)
       expect([2, 5, 8, 11].my_count(&:even?)).to eql(2)
     end
+
+    it 'should not return the wrong number of element in an array' do
+      expect([3, 5, 7].my_count).not_to eq(5)
+      expect([2, 5, 8, 11].my_count(&:even?)).not_to eql(4)
+      expect([3, 5, 3, 7].my_count(3)).not_to eq(1)
+    end
   end
 
   describe 'my_map' do
@@ -119,6 +138,12 @@ describe Enumerable do
       expect([2, 4, 6].my_map { |num| num * 3 }).to eq([6, 12, 18])
       expect(%w[hi hello holla].my_map(&:upcase)).to eq(%w[HI HELLO HOLLA])
       expect([2, 5, 8].my_map(&:even?)).to eql([true, false, true])
+    end
+
+    it 'should not return a new array that all element does not satisfies the condition in the block' do
+      expect([2, 4, 6].my_map { |num| num * 3 }).not_to eq([9, 12, 18])
+      expect(%w[hi hello holla].my_map(&:upcase)).not_to eq(%w[Hi HELLO HOLLA])
+      expect([2, 5, 8].my_map(&:even?)).not_to eql([false, false, true])
     end
   end
 
@@ -131,6 +156,11 @@ describe Enumerable do
     it 'should accept a symbol and also accept a symbol with an integer' do
       expect((5..10).my_inject(:+)).to eq(45)
       expect((5..10).my_inject(2, :*)).to eq(302_400)
+    end
+
+    it 'should not return the wrong total of all the element' do
+      expect([2, 4, 6].my_inject { |sum, n| sum + n }).not_to eq(10)
+      expect([2, 4, 6].my_inject { |product, n| product * n }).not_to eq(12)
     end
   end
 end
